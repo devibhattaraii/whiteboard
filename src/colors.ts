3a04fa3547078fec37c84196c2ed4e67c74475d9
Devi Bhattarai <devakabhattaraii@gmail.com>
2021-08-14 20:28:29 +0545

    Color palette finalized for lower

--- src/colors.ts
+++ src/colors.ts
@@ -2,21 +2,30 @@ import oc from "open-color";
 
 const shades = (index: number) => [
   oc.red[index],
-  oc.pink[index],
-  oc.grape[index],
-  oc.violet[index],
-  oc.indigo[index],
+  // oc.pink[index],
+  // oc.grape[index],
+  // oc.violet[index],
+  // oc.indigo[index],
   oc.blue[index],
-  oc.cyan[index],
-  oc.teal[index],
+  // oc.cyan[index],
+  // oc.teal[index],
   oc.green[index],
-  oc.lime[index],
+  // oc.lime[index],
   oc.yellow[index],
-  oc.orange[index],
+  // oc.orange[index],
+];
+
+const themeShades = (index: number) => [
+  oc.blue[index],
+  // oc.cyan[index],
+  // oc.teal[index],
+  // oc.orange[index],
 ];
 
 export default {
-  canvasBackground: [oc.white, oc.gray[0], oc.gray[1], ...shades(0)],
-  elementBackground: ["transparent", oc.gray[4], oc.gray[6], ...shades(6)],
-  elementStroke: [oc.black, oc.gray[8], oc.gray[7], ...shades(9)],
+  canvasBackground: [oc.white, oc.gray[1], ...themeShades(0)],
+  elementBackground: ["transparent", oc.gray[4], ...shades(6)],
+  elementStroke: [oc.black, ...shades(9)],
 };
+
+// S4EF1
